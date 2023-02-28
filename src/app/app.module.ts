5282daad63fe233e8506311efa6293e020961e76
assaduzzaman <assad.zaman@gmail.com>
2023-02-28 19:42:38 +0600

    aadfadf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,10 +27,3 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
   ]
 })
 export class AppModule { }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
